6977ec4abef1108001e7e29742778507b7196ea8
Gian Avila <avila.gian111@gmail.com>
2025-02-16 11:37:14 +0700

    fix: add base path configuration to Vite config files

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import generouted from "@generouted/react-router/plugin";
 import basicSsl from "@vitejs/plugin-basic-ssl";
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), generouted(), basicSsl()],
-  base: './',
-  resolve: {
-    alias: {
-      "@": "/src",
+    plugins: [react(), generouted(), basicSsl()],
+    base: './',
+    resolve: {
+        alias: {
+            "@": "/src",
+        },
     },
-  },
 });
